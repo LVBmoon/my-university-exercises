e5135f4e3fb68e01ca9d93325e6f274c0e336d50
LVBmoon <mobinaseif84@gmail.com>
2024-12-06 10:02:55 +0330

    Update tamrin7-the-is-prime-code.cpp

--- tamrin7-the-is-prime-code.cpp
+++ tamrin7-the-is-prime-code.cpp
@@ -1,30 +1,4 @@
 //write an algoritm which can find prime numbers from 0 to a number chosed by user
-//first(I think this way has some bugs):
-/*#include <iostream>
-using namespace std;
-int main() {
-    int number , q = 2, a = 1 ;
-    float jazr_number;
-    bool prime = true;
-    cout << "please enter a number: ";
-    cin >> number;
-    while(q<=number){
-        jazr_number = (0.5)*(a+(number/a));
-        while((q<=number)&&(prime==true)){
-            if(number % q == 0){
-                prime = false;
-            }
-            q++;
-        }
-        if(prime == true){
-            cout << "number is: " << number;
-        }
-        q++;
-    }
-    return 0;
-    }*/
-
-//second way:
 #include <iostream>
 using namespace std;
 
